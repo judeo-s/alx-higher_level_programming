6cceab09a612d3d272bcacdfafaf621626665a9a
JudeO-S <jude@example.com>
2024-01-02 23:11:19 +0000

    Done with task 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def pow(a: int, b: int) -> int:
+    '''
+    A function that computes a to the power of b and return the value
+
+    Args:
+        a:int
+        b:int
+    Returns:
+        int
+    '''
+    return a ** b
