e1577bc06060378184a700594a4be724b424b263
JudeO-S <jude@example.com>
2024-01-02 23:10:45 +0000

    Done with task 10

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add(a: int, b: int) -> int:
+    '''
+    A function that adds two integers and returns the result
+
+    Args:
+        a:int
+        b:int
+    Returns:
+        int
+    '''
+    return a + b
