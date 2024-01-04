ad278ef6c2327f38ada090c4df727b2a64f1fa06
JudeO-S <jude@example.com>
2024-01-04 23:24:38 +0000

    Done with task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,11 +2,11 @@
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    a = 10
-    b = 5
-    operators = {"+": add, "-": sub, "*": mul, "/": div}
+	ops = {"+": add, "-": sub, "*": mul, "/": div}
+	a = 10
+	b = 5
 
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, calculator(a, b)))
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, calculator(a, b)))
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, calculator(a, b)))
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, calculator(a, b)))
+	print("{:d} + {:d} = {:d}".format(a, b, ops.get("+")(a, b)))
+	print("{:d} - {:d} = {:d}".format(a, b, ops.get("-")(a, b)))
+	print("{:d} * {:d} = {:d}".format(a, b, ops.get("*")(a, b)))
+	print("{:d} / {:d} = {:d}".format(a, b, ops.get("/")(a, b)))
