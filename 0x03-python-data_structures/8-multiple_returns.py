7484ab88f557768bda40e6a9387ecdb673277edc
JudeO-S <jude@example.com>
2024-01-08 07:53:19 +0000

    Done with task 8

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    '''
+    a function that returns a tuple with the length of a string
+    and its first character.
+
+    Args:
+        sentence : str
+    Returns:
+        length : int
+        character : str
+    '''
+    if sentence:
+        return len(sentence), sentence[0]
+    else:
+        return None
