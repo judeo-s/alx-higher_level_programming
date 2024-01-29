bb5f6efd1713ec8dfb43750e75e342256116f1fc
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-01-29 20:48:37 +0000

    Done with task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -39,7 +39,7 @@ class Rectangle:
         Raises:
             TypeError, ValueError
         """
-        if value not int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -57,7 +57,15 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if value not int:
+        """
+        Sets the value of the height of a Rectangle object.
+
+        Args:
+            value: int
+        Raises:
+            TypeError, ValueError
+        """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
