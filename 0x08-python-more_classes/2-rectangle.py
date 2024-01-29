49bfd25fe06a090cead49132bd55e86fea64838a
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-01-29 21:18:59 +0000

    fixed the logic on handling 0 values when calculating the perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -87,4 +87,6 @@ class Rectangle:
         Returns:
             int
         """
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return 2 * (self.__width + self.__height)
