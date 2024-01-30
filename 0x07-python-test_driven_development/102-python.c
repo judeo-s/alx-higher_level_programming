c8fffa1a56dff648e7a79cae82c5146e367f4998
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-01-30 22:34:04 +0000

    Done with task 8

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,31 @@
+#include <Python.h>
+
+/**
+ * print_python_string - Prints information about Python strings.
+ * @p: PyObject
+ */
+void print_python_string(PyObject *p)
+{
+	ssize_t size;
+
+	printf("[.] string object info\n");
+
+	if (strcmp((p->ob_type)->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+	{
+		printf("  type: compact ascii\n");
+	}
+	else
+	{
+		printf("  type: compact unicode object\n");
+	}
+
+	size = ((PyASCIIObject *)p)->length;
+	printf("  length: %ld\n", size);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &size));
+}
