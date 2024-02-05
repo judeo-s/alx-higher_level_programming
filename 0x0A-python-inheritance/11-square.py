8d60ce94fa3d33b022a0ed89ad35fa01ce8522e0
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-02-05 22:07:34 +0000

    made a mistake with the importation of the module

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,7 +6,7 @@ A module contains a class that models a square
 '''
 
 
-Rectangle = __import__("9-rectangle.py").Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
@@ -19,7 +19,7 @@ class Square(Rectangle):
         Args:
             size: int
         '''
-        self.integer_validator(None, size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
