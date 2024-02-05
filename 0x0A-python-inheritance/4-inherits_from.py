47f78ec8b147481c25fc1e1d1aeed1813c47227b
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-02-05 22:01:43 +0000

    fixed a few things with task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python3
+
+"""
+A module with a function that returns True if an object is an
+instance of a class that inherits from the specified class.
+"""
+
+
 def inherits_from(obj, a_class):
     '''
     A function that returs True if the object is an instance of a class
