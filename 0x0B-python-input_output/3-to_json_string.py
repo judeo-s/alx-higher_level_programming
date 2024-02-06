b1e35fa20c34d4743488c5a9abb5c575c4087610
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-02-06 17:42:50 +0000

    Done with task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import json
+
+
+''' A module that has a function that returns a json format of an object '''
+
+
+def to_json_string(my_obj):
+    '''
+    A function that returns the JSON representation of an object (string)
+
+    Args:
+        my_obj: class
+    Returns:
+        str
+    '''
+    return json.dumps(my_obj)
