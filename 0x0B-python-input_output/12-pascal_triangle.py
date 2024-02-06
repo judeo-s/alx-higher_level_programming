83a4de1ab29e31d27305cdc3255ed919dd3a23ce
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-02-06 21:05:54 +0000

    done with task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""Solves the Pascal's triangle problem"""
+
+
+def pascal_triangle(n: int) -> "list[list[int]]":
+    """
+    Returns a list of lists of integers representing ou arethe Pascal's
+    triangle up to height `n`.
+
+    Args:
+        n: int
+    Returns:
+        list
+    """
+    if n <= 0:
+        return []
+    p_triangle = [[1]]
+    for _ in range(n - 1):
+        current_row = p_triangle[-1]
+        new_row = (
+            [1]
+            + [
+                current_row[i] + current_row[i + 1]
+                for i in range(len(current_row) - 1)
+            ]
+            + [1]
+        )
+        p_triangle.append(new_row)
+    return p_triangle
