2bf2f20c0b63991a25f2d1e012f9bc6b749b1269
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-02-06 20:50:10 +0000

    done with task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""A module that models a Student object."""
+
+
+class Student:
+    """Models a Student object."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes and creates a new Student object.
+
+        Args:
+            first_name: str
+            last_name: str
+            age: int
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self) -> dict:
+        """Returns the dictionary representation of the Student object."""
+        if attr is not None and isinstance(attr, list):
+            return (
+                {
+                    key: value for key, value in self.__dict__.items()
+                    if key in attr
+                }
+            )
+        return self.__dict__
