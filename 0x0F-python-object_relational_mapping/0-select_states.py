2a817557e2516d373dba74c7cb404ac794dde52b
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-04-14 08:52:40 +0100

    done with task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+A module to select data from the states table
+"""
+import sys
+import MySQLdb
+
+db = MySQLdb.connect(
+        host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+        )
+cursor = db.cursor()
+try:
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
+except MySQLdb.Error as e:
+    try:
+        print(f"MySQL Error [e.args[0]]: {e.args[1]}")
+    except IndexError:
+        print(f"MySQL Error: {str(e)}")
+for row in rows:
+    print(f"({row[0]}, '{row[1]}')")
+cursor.close()
+db.close()
