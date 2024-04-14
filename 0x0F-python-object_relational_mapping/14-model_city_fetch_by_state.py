3070e2059022a7364c653dcf7677c89aacd06452
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-04-15 00:16:47 +0100

    made a few changes to task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, select, MetaData
 from sqlalchemy.orm import sessionmaker
 import sys
 from model_state import Base, State
+from model_city import City
 
 if __name__ == "__main__":
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
