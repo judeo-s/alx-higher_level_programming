dff3da84a0f6b6348eeda9f4d42bae8130860796
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-04-14 09:05:03 +0100

    done with task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A module to select data from the states table using the where clause
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    Access database and retrieve specific data from the states table
+    """
+    db = MySQLdb.connect(
+            host='localhost', user=sys.argv[1], passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cursor = db.cursor()
+    try:
+        cursor.execute("SELECT * FROM states WHERE id > 3")
+        rows = cursor.fetchall()
+    except MySQLdb.Error as e:
+        try:
+            print(f"MySQL Error [e.args[0]]: {e.args[1]}")
+        except IndexError:
+            print(f"MySQL Error: {str(e)}")
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
