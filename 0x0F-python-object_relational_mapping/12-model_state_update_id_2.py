2c371f8d2c44a77374bd495defe19d261cf46536
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-04-15 00:02:00 +0100

    done with task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,11 +10,13 @@ from model_state import Base, State
 if __name__ == "__main__":
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3])
+
     engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    state = session.query(State).filter(State.id == '2').first()
-    state.name = "New Mexico"
+
+    ari_state = session.query(State).filter(State.id == '2').first()
+    ari_state.name = 'New Mexico'
     session.commit()
     session.close()
