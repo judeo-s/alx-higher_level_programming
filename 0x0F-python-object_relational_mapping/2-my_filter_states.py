9b7cc0e46485a5fb41cb4a233a06d8713a6cd12e
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-04-14 09:45:05 +0100

    made a few changes to task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,16 +10,14 @@ if __name__ == "__main__":
     """
     Access database and retrieve specific data from the states table
     """
-    db = MySQLdb.connect(
-            host='localhost', user=sys.argv[1], passwd=sys.argv[2],
-            db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], port=3306,
+                         passwd=sys.argv[2], db=sys.argv[3])
 
-    cursor = db.cursor()
-    query = f"SELECT * FROM states \
-            WHERE name LIKE BINARY '{sys.argv[4]}' \
-            ORDER BY states.id ASC"
-    cursor.execute(query)
-    rows = cursor.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(sys.argv[4]))
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
