d45bc0dcda11aace5467994e89ebeca7817e3e52
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-04-14 22:35:39 +0100

    made a few changes to the code

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
 
     session = Session()
-
-    instance =  session.query(State).order_by(State.id).first()
+    instance = session.query(State).order_by(State.id).first()
 
     if not instance:
         print("Nothing")
