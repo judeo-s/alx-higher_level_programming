529746ff5b10c6dee6d1eefcc11cfa8c7ff4bc89
Jude Osam-Sackey <43099552+judeo-s@users.noreply.github.com>
2024-05-21 10:55:01 +0000

    fixed a few issues with the coding style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,14 +4,13 @@
 A script that reads and prints the content of a file.
 */
 
-
 const fs = require('fs');
-const args = process.argv
+const args = process.argv;
 
 fs.readFile(args[2], 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
